9ef8cbe7db530be7376795bc11a15b0837fcb7ed
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-04-05 11:08:12 -0400

    more setup.

--- review/src/index.js
+++ review/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
-import AppFunc from './AppFunc';
-import AppClass from './AppClass';
 import App from './App';
 
 const rootElement = document.getElementById('root');
